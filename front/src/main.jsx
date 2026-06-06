a62cb1a24486bad85d26d1945f9343f7bf779107
GKTNgames <guilhermektaba@gmail.com>
2026-06-06 13:11:07 -0300

    fix: rotas prootegidas

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,8 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router'
 
+const PrivateRoute = ({ element }) => {
+  const token = localStorage.getItem("token");
+  return token ? element : <Navigate to="/" replace />;
+};
+
 const AdminRoute = ({ element }) => {
+  const token = localStorage.getItem("token");
   const cargo = localStorage.getItem("cargo");
+  if (!token) return <Navigate to="/" replace />;
   return cargo === "ADMIN" ? element : <Navigate to="/dashboard" replace />;
 };
 import './index.css'
@@ -31,35 +38,36 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/"                   element={<Login />} />
-        <Route path="/dashboard"          element={<Dashboard />} />
-        <Route path="/familias"           element={<Familias />} />
-        <Route path="/novo-cadastro"                  element={<NovosCadastro />} />
-        <Route path="/novo-cadastro/:id"            element={<NovosCadastro />} />
-        <Route path="/novo-cadastro/:id/:fichaId"   element={<NovosCadastro />} />
-        <Route path="/atendimentos"       element={<Atendimentos />} />
-        <Route path="/novo-atendimento"                                   element={<NovoAtendimento />} />
-        <Route path="/novo-atendimento/:familiaId"                      element={<NovoAtendimento />} />
-        <Route path="/novo-atendimento/:familiaId/:atendimentoId"       element={<NovoAtendimento />} />
-        <Route path="/agenda"             element={<Agenda />} />
-        <Route path="/painel-admin"          element={<AdminRoute element={<PainelAdmin />} />} />
-        <Route path="/detalhes-familia/:id"   element={<DetalhesFamilia />} />
-        <Route path="/ficha-atualizacao/:id"                      element={<FichaAtualizacao />} />
-        <Route path="/ficha-atualizacao/:id/:fichaId"            element={<FichaAtualizacao />} />
-        <Route path="/termo-imagem/:id"                            element={<TermoImagem />} />
-        <Route path="/termo-imagem/:id/:termoId"                  element={<TermoImagem />} />
-
-        <Route path="/ficha-visita/:id"                            element={<FichaVisitaDomiciliar />} />
-        <Route path="/ficha-visita/:id/:fichaId"                  element={<FichaVisitaDomiciliar />} />
-        <Route path="/plano-desenvolvimento/:id"                   element={<PlanoDesenvolvimento />} />
-        <Route path="/plano-desenvolvimento/:id/:planoId"         element={<PlanoDesenvolvimento />} />
-        <Route path="/plano-pdu/:id"                               element={<PlanoDesenvolvimentoPDU />} />
-        <Route path="/plano-pdu/:id/:planoId"                    element={<PlanoDesenvolvimentoPDU />} />
-        <Route path="/folha-prosseguimento/:id"                    element={<FolhaProsseguimento />} />
-        <Route path="/folha-prosseguimento/:id/:folhaId"          element={<FolhaProsseguimento />} />
-        <Route path="/criar-conta"            element={<CriarConta />} />
         <Route path="/esqueci-senha"          element={<EsqueciSenha />} />
         <Route path="/redefinir-senha"        element={<RedefinirSenha />} />
-        <Route path="*"                      element={<Navigate to="/" replace />} />
+
+        <Route path="/dashboard"          element={<PrivateRoute element={<Dashboard />} />} />
+        <Route path="/familias"           element={<PrivateRoute element={<Familias />} />} />
+        <Route path="/novo-cadastro"                  element={<PrivateRoute element={<NovosCadastro />} />} />
+        <Route path="/novo-cadastro/:id"              element={<PrivateRoute element={<NovosCadastro />} />} />
+        <Route path="/novo-cadastro/:id/:fichaId"     element={<PrivateRoute element={<NovosCadastro />} />} />
+        <Route path="/atendimentos"       element={<PrivateRoute element={<Atendimentos />} />} />
+        <Route path="/novo-atendimento"                                   element={<PrivateRoute element={<NovoAtendimento />} />} />
+        <Route path="/novo-atendimento/:familiaId"                        element={<PrivateRoute element={<NovoAtendimento />} />} />
+        <Route path="/novo-atendimento/:familiaId/:atendimentoId"         element={<PrivateRoute element={<NovoAtendimento />} />} />
+        <Route path="/agenda"             element={<PrivateRoute element={<Agenda />} />} />
+        <Route path="/painel-admin"       element={<AdminRoute element={<PainelAdmin />} />} />
+        <Route path="/detalhes-familia/:id"           element={<PrivateRoute element={<DetalhesFamilia />} />} />
+        <Route path="/ficha-atualizacao/:id"          element={<PrivateRoute element={<FichaAtualizacao />} />} />
+        <Route path="/ficha-atualizacao/:id/:fichaId" element={<PrivateRoute element={<FichaAtualizacao />} />} />
+        <Route path="/termo-imagem/:id"               element={<PrivateRoute element={<TermoImagem />} />} />
+        <Route path="/termo-imagem/:id/:termoId"      element={<PrivateRoute element={<TermoImagem />} />} />
+
+        <Route path="/ficha-visita/:id"               element={<PrivateRoute element={<FichaVisitaDomiciliar />} />} />
+        <Route path="/ficha-visita/:id/:fichaId"      element={<PrivateRoute element={<FichaVisitaDomiciliar />} />} />
+        <Route path="/plano-desenvolvimento/:id"      element={<PrivateRoute element={<PlanoDesenvolvimento />} />} />
+        <Route path="/plano-desenvolvimento/:id/:planoId" element={<PrivateRoute element={<PlanoDesenvolvimento />} />} />
+        <Route path="/plano-pdu/:id"                  element={<PrivateRoute element={<PlanoDesenvolvimentoPDU />} />} />
+        <Route path="/plano-pdu/:id/:planoId"         element={<PrivateRoute element={<PlanoDesenvolvimentoPDU />} />} />
+        <Route path="/folha-prosseguimento/:id"       element={<PrivateRoute element={<FolhaProsseguimento />} />} />
+        <Route path="/folha-prosseguimento/:id/:folhaId" element={<PrivateRoute element={<FolhaProsseguimento />} />} />
+        <Route path="/criar-conta"        element={<PrivateRoute element={<CriarConta />} />} />
+        <Route path="*"                   element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
